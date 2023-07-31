ea900ec91ce5c1972f7de709ffa60f1fbe7a51da
MikkoDT <mikko.detorres@g.batstate-u.edu.ph>
2023-07-31 18:08:28 +0800

    add mymodule.py

---
+++ mymodule.py
@@ -0,0 +1,9 @@
+# welcome example
+def welcome(name):
+    print("Hello, "+ name +" to Analytics Vidhya")
+
+# dictionary and variable example
+person1 = {"name":"Chirag Goyal",
+           "age":19,
+           "country":"India",
+           "education":"IIT Jodhpur"}

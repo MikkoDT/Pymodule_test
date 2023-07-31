32bc0eefbb5ebf506bf82ee25395efe1de4b4a26
MikkoDT <mikko.detorres@g.batstate-u.edu.ph>
2023-07-31 18:13:51 +0800

    add folder Test_codes

---
+++ Test_codes/from_mymodule.py
@@ -0,0 +1,3 @@
+from mymodule import person1
+
+print("a = ",person1["age"])
---
+++ Test_codes/test_mymodule.py
@@ -0,0 +1,12 @@
+import mymodule
+
+#welcome example
+mymodule.welcome("Chirag Goyal")
+
+#dictionary and variable example
+a = mymodule.person1["age"]
+b = mymodule.person1["education"]
+c = mymodule.person1["country"]
+print("a = ",a)
+print("b = ",b)
+print("c = ",c)
